7eb58e9df8b41611312c3310b4846384b68601c2
Dave Billin <david.billin@vandals.uidaho.edu>
2016-03-27 22:11:55 -0700

    Add libproj CMake find module to IvP.  Fix up build scripts some more

--- build-ivp.sh
+++ build-ivp.sh
@@ -1,66 +1,93 @@
 #!/bin/bash
+#====================================================================
+# This script builds IvP utilities and libraries in an out-of-source
+# build.  Build artifacts (CMake files, etc.) are written to
+# <SCRIPT_DIR>/build/MOOS, where SCRIPT_DIR indicates the directory
+# where this script resides.  Similarly, all binaries (libraries, 
+# executables) are written to <SCRIPT_DIR>/bin
+#====================================================================
+set -o nounset   # Disallow unset variables
+set -o errexit   # Exit on error
+#set -o xtrace    # Print commands as they are executed
 
-BUILD_DEBUG="yes"  
-BUILD_OPTIM="yes"
-CLEAN="no"
-CMD_ARGS=""
-BUILD_GUI_CODE="ON"
-BUILD_BOT_CODE_ONLY="OFF"
+_script_name="$(basename ${0})"
+_script_dir="$(readlink -f $(dirname ${0}))"
 
 
+
+#--------------------------------------------------------------------
+# DESCRIPTION: Prints script usage and exits
+#--------------------------------------------------------------------
 print_usage_and_exit()
 {
-    printf "build-ivp.sh [OPTIONS] [MAKE ARGS]            \n"
-    printf "Options:                                      \n"
-    printf "  --help, -h                                  \n"
-    printf "  --nodebug                                   \n"
-    printf "    Do not include the -g compiler flag       \n"
-    printf "  --noopt                                     \n"
-    printf "    Do not include the -O3 compiler flag      \n"
-    printf "  --fast, -f                                  \n"
-    printf "    Do not include the -O3, -g compiler flags \n"
-    printf "  --nogui, -n                                 \n"
-    printf "    Do not build GUI related apps             \n"
-    printf "  --minrobot, -m                              \n"
-    printf "    Only build minimal robot apps             \n"
-    printf "    (Even smaller subset than with --nogui)   \n"
-    printf "  --clean, -c                                 \n"
-    printf "    Invokes make clean and removes build/*    \n"
-    printf "                                              \n"
-    printf "By default, all code is built, and the debug and optimization  \n"
-    printf "compiler flags are invoked.                                    \n"
-    printf "                                                               \n"
-    printf "Note: By default -j12 is provided to make to utilize up to 12  \n"
-    printf "      processors in the build. This can be overridden simply   \n"
-    printf "      by using -j1 on the command line instead. This will give \n"
-    printf "      more reasonable output if there should be a build error. \n"
+echo "
+USAGE:  ${_script_name} [OPTIONS] [MAKE ARGS]
+
+OPTIONS:
+  --help, -h       Print usage info and exit
+  --nodebug        Build without debug symbols
+  --noopt          Build with no compiler optimizations
+  --fast, -f       Build without optimization or debug symbols
+  --nogui, -n      Do not build GUI-related apps
+  --minrobot, -m   Build only minimal robot apps (smaller than --nogui)
+  --purge, -p      Remove existing build artifacts before building
+
+By default, all code is built, and the debug and optimization
+compiler flags are invoked.
+
+Note: By default -j12 is provided to make to utilize up to 12
+      processors in the build. This can be overridden simply
+      by using -j1 on the command line instead. This will give
+      more reasonable output if there should be a build error.
+"
     exit 1
 }
 
+
+#===================================
+# Parse command line arguments
+#===================================
+purge_requested='no'
+BUILD_DEBUG="yes"  
+BUILD_OPTIM="yes"
+CMD_ARGS=
+BUILD_GUI_CODE="ON"
+BUILD_BOT_CODE_ONLY="OFF"
+
 for ARGI; do
-    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
-        print_usage_and_exit;
-    elif [ "${ARGI}" = "--nodebug" ] ; then
-        BUILD_DEBUG="no"
-    elif [ "${ARGI}" = "--noopt" ] ; then
-        BUILD_OPTIM="no"
-    elif [ "${ARGI}" = "--fast" -o "${ARGI}" = "-f" ] ; then
-        BUILD_DEBUG="no"
-        BUILD_OPTIM="no"
-    elif [ "${ARGI}" = "--clean" -o "${ARGI}" = "-c" ] ; then
-        CLEAN="yes"
-    elif [ "${ARGI}" = "--nogui" -o "${ARGI}" = "-n" ] ; then
-        BUILD_GUI_CODE="no"
-    elif [ "${ARGI}" = "--minrobot" -o "${ARGI}" = "-m" ] ; then
-        BUILD_BOT_CODE_ONLY="ON"
-	BUILD_GUI_CODE="OFF"
-    else
-	if [ "$CMD_ARGS" = "" ] ; then
-	    CMD_ARGS=$ARGI
-	else
-	    CMD_ARGS=$CMD_ARGS" "$ARGI
-	fi
-    fi
+   case ${ARGI} in
+      --help|-h)
+           print_usage_and_exit;;
+
+      --nodebug)
+           BUILD_DEBUG="no";;
+
+      --noopt)
+           BUILD_OPTIM="no";;
+
+      --fast|-f)
+           BUILD_DEBUG="no"
+           BUILD_OPTIM="no";;
+
+      --purge|-p)
+           purge_requested='yes';;
+
+      --nogui|-n)
+           BUILD_GUI_CODE="no";;
+
+      --minrobot|-m)
+           BUILD_BOT_CODE_ONLY="ON"
+           BUILD_GUI_CODE="OFF";;
+
+      *)   set +o nounset
+           if [ -z "${CMD_ARGS}" ]
+           then
+              CMD_ARGS+=" ${ARGI}"
+           else
+              CMD_ARGS=$CMD_ARGS" "$ARGI
+           fi
+           set -o nounset;;
+   esac
 done
 
 ################################################################################
@@ -72,63 +99,68 @@ if [ ${BUILD_OPTIM} = "yes" ] ; then
     CMAKE_CXX_FLAGS=$CMAKE_CXX_FLAGS" -O3"
 fi
 
-printf "Compiler flags: ${CMAKE_CXX_FLAGS}  \n\n" 
 
-
-################################################################################
-INVOCATION_ABS_DIR="`pwd`"
-
-BLD_ABS_DIR="${INVOCATION_ABS_DIR}/build"
-LIB_ABS_DIR="${INVOCATION_ABS_DIR}/lib"
-BIN_ABS_DIR="${INVOCATION_ABS_DIR}/bin"
-SRC_ABS_DIR="${INVOCATION_ABS_DIR}/ivp/src"
-
-printf "Built files will be placed into these directories: \n"
-printf "  Intermediate build files: ${BLD_ABS_DIR}         \n"
-printf "  Libraries:                ${LIB_ABS_DIR}         \n"
-printf "  Programs:                 ${BIN_ABS_DIR}       \n\n"
-
-mkdir -p "${BLD_ABS_DIR}"
-mkdir -p "${LIB_ABS_DIR}"
-mkdir -p "${BIN_ABS_DIR}"
-
-cd "${BLD_ABS_DIR}"
 
 ################################################################################
 # For back compatability, if user has environment variable IVP_BUILD_GUI_CODE 
 # set to "OFF" then honor it here as if --nogui were set on the command line
 
-if [ ${IVP_BUILD_GUI_CODE} = "OFF" ] ; then
+set +o nounset
+if [ "${IVP_BUILD_GUI_CODE}" = "OFF" ] ; then
     BUILD_GUI_CODE="OFF"
-    printf "IVP GUI Apps will not be built. IVP_BUILD_GUI_CODE env var is OFF\n"
+    echo "IVP GUI Apps will not be built. IVP_BUILD_GUI_CODE env var is OFF"
 fi
+set -o nounset
 
-printf "BUILD_GUI_CODE = ${BUILD_GUI_CODE} \n"
 
-################################################################################
-printf "Invoking cmake...\n"
-
-printf "BUILD_BOT_CODE_ONLY: ${BUILD_BOT_CODE_ONLY}   \n"
-
-cmake -DIVP_BUILD_GUI_CODE=${BUILD_GUI_CODE}               \
-      -DIVP_BUILD_BOT_CODE_ONLY=${BUILD_BOT_CODE_ONLY}     \
-      -DIVP_LIB_DIRECTORY="${LIB_ABS_DIR}"                 \
-      -DIVP_BIN_DIRECTORY="${BIN_ABS_DIR}"                 \
-      -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}"               \
-      -DUSE_UTM=ON                                         \
-      ${IVP_CMAKE_FLAGS}                                   \
-      "${SRC_ABS_DIR}"
 
 ################################################################################
-printf "Invoking make ${CMD_ARGS}\n"
+_build_dir="${_script_dir}/build/IvP"
+_bin_dir="${_script_dir}/bin"
+_lib_dir="${_script_dir}/lib"
+_ivp_src_dir="${_script_dir}/ivp/src"
 
-if [ "${CLEAN}" = "yes" -o "${CMD_ARGS}" = "clean" ] ; then
-    printf "CLEANING....\n"
-    make clean
-    cd ${INVOCATION_ABS_DIR}
-    rm -rf build/*
+echo "
+Compiler flags: ${CMAKE_CXX_FLAGS}
+BUILD_GUI_CODE = ${BUILD_GUI_CODE}
+BUILD_BOT_CODE_ONLY: ${BUILD_BOT_CODE_ONLY}
+
+Build artifacts will be written to the following directories:
+Intermediate build files: ${_build_dir}
+Libraries:                ${_lib_dir}
+Programs:                 ${_bin_dir}
+"
+
+# Create build and output directories
+mkdir -p "${_build_dir}"
+mkdir -p "${_lib_dir}"
+mkdir -p "${_bin_dir}"
+
+
+# Implement purge
+if [ "${purge_requested}" = 'yes' ]
+then
+    echo "Purging build artifacts in '${_build_dir}'"
+    (cd ${_build_dir} && make clean) || true
+    rm -rf ${_build_dir}
+    exit 0
 else
-    make -j12 ${CMD_ARGS}
+   (
+   cd ${_build_dir}
+   echo "Configuring CMake..."
+   cmake -DIVP_BUILD_GUI_CODE=${BUILD_GUI_CODE} \
+         -DIVP_BUILD_BOT_CODE_ONLY=${BUILD_BOT_CODE_ONLY} \
+         -DIVP_LIB_DIRECTORY="${_lib_dir}" \
+         -DIVP_BIN_DIRECTORY="${_bin_dir}" \
+         -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
+         -DUSE_UTM=ON \
+         "${_ivp_src_dir}" \
+            >/dev/null
+ 
+#         -DPROJ4_INCLUDE_SEARCH_PATHS="${_script_dir}/MOOS/proj-4.8.0" \
+#         -DPROJ4_LIBRARY_SEARCH_PATHS="${_lib_dir}" \
+   echo "Running 'make ${CMD_ARGS}'"
+   make -j12 ${CMD_ARGS}
+   )
 fi
 
-cd ${INVOCATION_ABS_DIR}
--- build-moos.sh
+++ build-moos.sh
@@ -15,41 +15,87 @@ _script_dir="$(readlink -f $(dirname ${0}))"
 
 _build_dir="${_script_dir}/build/MOOS"
 _bin_dir="${_script_dir}/bin"
-
+_lib_dir="${_script_dir}/lib"
 
 cmake_build_type="RelWithDebInfo"
-cmd_args=
+cmd_args="-j12"		# Default to building with up to 12 cores
 
+
+
+
+
+#--------------------------------------------------------------------
+# DESCRIPTION: Prints script usage and exits
+#--------------------------------------------------------------------
+print_usage_and_exit ()
+{
+echo "
+USAGE: ${_script_name} [OPTIONS] [make arguments...]
+
+OPTIONS:
+   --help, -h         Print usage and exit
+   --debug, -d        Build binaries with debug symbols and no optimization
+   --release, -r      Build optimized binaries with debug symbols
+   --minrelease, -m   Build optimized binaries with no debug symbols
+   --purge, -p        Purge existing build artifacts (build from scratch)
+
+make arguments:
+   Arguments not included in OPTIONS are passed directly to make
+
+Recommended make arguments
+ -jN    Build using N parallel threads (if not specified, -j12 is used)
+ -k     Keep building after failed component
+ clean  Run 'make clean' before compiling
+"
+
+exit 0
+}
+
+#--------------------------------------------------------------------
+# DESCRIPTION: Purges a specified build directory's build artifacts
+# PARAM: The build directory to purge
+#--------------------------------------------------------------------
+purge_build_directory ()
+{
+   local target_dir="${@}"
+
+   if [ -d "${target_dir}" ]
+   then
+      echo "Purging build artifacts in directory '${target_dir}'"
+      (cd "${target_dir}" && make clean >/dev/null) || true
+      rm -rf ${target_dir} || true
+   fi
+}
+
+bar="================================================================"
+
+
+#===================================
+# Parse command line arguments
+#===================================
+purge_requested='no'
 for ARGI; do
     case ${ARGI} in
        --help|-h)
-          printf "%s [SWITCHES]  \n" $0
-          printf "  --help, -h                                   \n"
-          printf "  --debug, -d                                  \n"
-          printf "  --release, -r                                \n"
-          printf "Any other switches passed directly to \"make\" \n"
-          printf "Recommended:                                   \n"
-          printf " -j12   Speed up compile on multi-core machines. \n"
-          printf " -k     Keep building after failed component.    \n"
-          printf " clean  Clean/remove any previous build.         \n"
-          exit 0;;
+          print_usage_and_exit;;
    
       --debug|-d)
           cmake_build_type="Debug";;
    
       --release|-r)
           cmake_build_type="Release";;
-   
-      clean)
-          echo "Cleaning..."
-          rm -rf ${_build_dir}/* >/dev/null;;
-   
-      *) cmd_args+="${ARGI}";;
+  
+      --minrelease|-m)
+          cmake_build_type="MinSizeRel";;
+ 
+      --purge|-p)
+          purge_requested='yes';;
+ 
+      *) cmd_args+=" ${ARGI}";;
    esac
 done
 
 
-bar="================================================================"
 
 # Determine host platform
 build_platform=
@@ -66,16 +112,17 @@ ${bar}
 "
 
 
-# Set up the build directory
-mkdir -p ${_build_dir}
-
+# Set up the build directory and output directories
+mkdir -p "${_build_dir}"
+mkdir -p "${_bin_dir}"
+mkdir -p "${_lib_dir}"
  
 
 cmake_build_args=( 
                    "-DCMAKE_BUILD_TYPE=${cmake_build_type}"
-                   "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=${_bin_dir}"
-                   "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=${_bin_dir}"
-                   "-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY=${_bin_dir}"
+                   "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY='${_bin_dir}'"
+                   "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY='${_lib_dir}'"
+                   "-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY='${_lib_dir}'"
                  )
 
 
@@ -87,21 +134,27 @@ moos_core_build_dir="${_build_dir}/MOOSCore"
 echo "${bar}
 ***  Building MOOSCore  ***
 ${bar}"
-(
-mkdir -p ${moos_core_build_dir}
-cd ${moos_core_build_dir}
-echo "Configuring CMake..."
-cmake ${cmake_build_args[@]} \
-      -DENABLE_EXPORT=ON \
-      -DUSE_ASYNC_COMMS=ON \
-      -DTIME_WARP_AGGLOMERATION_CONSTANT=0.4 \
-      ${moos_core_dir} >/dev/null
 
-echo "
-Invoking make..."
-make ${cmd_args}
-)
-
+# Implement purge
+if [ "${purge_requested}" = 'yes' ]
+then 
+   purge_build_directory "${moos_core_build_dir}"
+else
+   (
+   mkdir -p "${moos_core_build_dir}"
+   cd "${moos_core_build_dir}"
+   echo "Configuring CMake..."
+   cmake ${cmake_build_args[@]} \
+         -DENABLE_EXPORT=ON \
+         -DUSE_ASYNC_COMMS=ON \
+         -DTIME_WARP_AGGLOMERATION_CONSTANT=0.4 \
+         "${moos_core_dir}" >/dev/null
+   
+   echo "
+   Invoking make..."
+   make ${cmd_args}
+   )
+fi
 
 #===================================================================
 # Part #2:  BUILD ESSENTIALS
@@ -112,19 +165,25 @@ echo -e "\n\n
 ${bar}
 ***  Building MOOSEssentials  ***
 ${bar}"
-(
-mkdir -p ${moos_essentials_build_dir}
-cd ${moos_essentials_build_dir}
-echo "Configuring CMake..."
-cmake ${cmake_build_args[@]} \
-      ${moos_essentials_dir} \
-         >/dev/null
 
-echo "
-Invoking make..."
-make ${cmd_args}
-)
-
+# Implement purge
+if [ "${purge_requested}" = 'yes' ] 
+then
+   purge_build_directory "${moos_essentials_build_dir}"
+else
+   (
+   mkdir -p "${moos_essentials_build_dir}"
+   cd "${moos_essentials_build_dir}"
+   echo "Configuring CMake..."
+   cmake ${cmake_build_args[@]} \
+         "${moos_essentials_dir}" \
+            >/dev/null
+   
+   echo "
+   Invoking make..."
+   make ${cmd_args}
+   )
+fi
 
 #===================================================================
 # Part #3:  BUILD MOOS GUI TOOLS
@@ -135,22 +194,28 @@ echo -e "\n\n
 ${bar}
 ***  Building MOOS GUI Tools  ***
 ${bar}"
-(
-mkdir -p ${moos_gui_tools_build_dir}
-cd ${_build_dir}
-echo "Configuring CMake..."
-cmake -DBUILD_CONSOLE_TOOLS=ON \
-      -DBUILD_GRAPHICAL_TOOLS=ON \
-      -DBUILD_UPB=ON \
-      ${cmake_build_args[@]} \
-      ${moos_gui_tools_dir} \
-         >/dev/null
 
-echo "
-Invoking make..."
-make ${cmd_args}
-)
-
+# Implement purge
+if [ "${purge_requested}" = 'yes' ] 
+then
+   purge_build_directory "${moos_gui_tools_build_dir}"
+else
+   (
+   mkdir -p "${moos_gui_tools_build_dir}"
+   cd "${_build_dir}"
+   echo "Configuring CMake..."
+   cmake -DBUILD_CONSOLE_TOOLS=ON \
+         -DBUILD_GRAPHICAL_TOOLS=ON \
+         -DBUILD_UPB=ON \
+         ${cmake_build_args[@]} \
+         "${moos_gui_tools_dir}" \
+            >/dev/null
+   
+   echo "
+   Invoking make..."
+   make ${cmd_args}
+   )
+fi
 
 #===================================================================
 # Part #4:  BUILD PROJ4
@@ -161,56 +226,71 @@ echo -e "\n\n
 ${bar}
 ***  Building libproj4  ***
 ${bar}"
-(
-mkdir -p ${libproj4_build_dir}
-cd ${libproj4_build_dir}
 
-if [ -e lib/libproj.dylib ] && \
-   [ -e lib/libproj.a ] 
+# Implement purge
+# TODO: Presently, libproj4 binaries installed in the build tree by
+#       this script do not get deleted as part of a purge.  This 
+#       could be improved...
+if [ "${purge_requested}" = 'yes' ] 
 then
-   echo "Using system libproj4"
+   purge_build_directory "${libproj4_build_dir}"
 else
-   echo "Building libproj4 from included sources"
-   echo "Configuring libproj4..."
-   ${libproj4_dir}/configure \
-      --exec-prefix='' \
-      --libexecdir=${_bin_dir} \
-      --libdir=${_bin_dir} \
-      --bindir=${_bin_dir} \
-         >/dev/null
-   echo -e "Building libproj4..."
-   make ${cmd_args} >/dev/null
-   echo -e "Installing libproj4 in the build tree..."
-   make install >/dev/null
+   (
+   mkdir -p "${libproj4_build_dir}"
+   cd "${libproj4_build_dir}"
+   
+   if [ -e "/lib/libproj.dylib" ] && \
+      [ -e "/lib/libproj.a" ] 
+   then
+      echo "Using system libproj4"
+   else
+      echo "Building libproj4 from included sources"
+      echo "Configuring libproj4..."
+      "${libproj4_dir}/configure" \
+         --exec-prefix='' \
+         --libexecdir="${_bin_dir}" \
+         --libdir="${_lib_dir}" \
+         --bindir="${_lib_dir}" \
+            >/dev/null
+      echo -e "Building libproj4..."
+      make ${cmd_args} >/dev/null
+      echo -e "Installing libproj4 in the build tree..."
+      make install >/dev/null
+   fi
+   )
 fi
-)
-
 
 #===================================================================
 # Part #5:  BUILD MOOS GEODESY
 #===================================================================
 moos_geodesy_dir="${_script_dir}/MOOS/MOOSGeodesy"
-moos_geodesy_build_dir="${_build_dir}/MOOS/MOOSGeodesy"
+moos_geodesy_build_dir="${_build_dir}MOOSGeodesy"
 echo -e "\n\n
 ${bar}
 ***  Building MOOSGeodesy  ***
 ${bar}"
 
-(
-mkdir -p ${moos_geodesy_build_dir}
-cd ${moos_geodesy_build_dir}
-echo "Configuring CMake..."
-cmake -DPROJ4_INCLUDE_DIRS="${libproj4_dir}/include;${libproj4_build_dir}/include" \
-      -DPROJ4_LIB_PATH=${_bin_dir} \
-      -DENABLE_EXPORT=ON \
-      ${cmake_build_args} \
-      ${moos_geodesy_dir} \
-         >/dev/null
-
-echo "
-Invoking make..."
-make ${cmd_args}
-)
+# Implement purge
+if [ "${purge_requested}" = 'yes' ] 
+then
+   purge_build_directory "${moos_geodesy_build_dir}"
+else
+   (
+   mkdir -p "${moos_geodesy_build_dir}"
+   cd "${moos_geodesy_build_dir}"
+   echo "Configuring CMake..."
+   cmake -DPROJ4_INCLUDE_DIRS="${libproj4_dir}/src" \
+         -DPROJ4_LIB_PATH="${_lib_dir}" \
+         -DENABLE_EXPORT=ON \
+         ${cmake_build_args[@]} \
+         "${moos_geodesy_dir}" \
+            >/dev/null
+   
+   echo "
+   Invoking make..."
+   make ${cmd_args}
+   )
+fi
 
 echo -e "\n\nMOOS libraries were built successfully"
 echo "Binaries have been written to '${_bin_dir}'"
